5f5c3b417ed90b867ddaee2f5d58c70d49b0daa1
Mohamed Al-Hajri <c1almo00@students.oamk.fi>
2023-07-19 12:42:13 +0200

    login signup ui finished, form validation done, utils updated

--- src/components/AuthForm.tsx
+++ src/components/AuthForm.tsx
@@ -1,14 +1,16 @@
-import React from 'react';
-import { View, Text, TextInput, TouchableOpacity } from 'react-native';
+import React, { useState } from 'react';
+import { View, Text, TextInput, TouchableOpacity, StyleSheet, KeyboardAvoidingView, Platform } from 'react-native';
+import { validateConfirmPassword, validateEmail, validateName, validatePassword, validateRequired } from '../utils/validation';
 
 export type AuthFormProps = {
-  onSubmit: () => void;
+ onSubmit: (values: Record<string, string>) => void;
   buttonText: string;
   promptText: string;
   PromptActionText: string;
   onPromptActionPress: () => void;
   fields: { label: string; fieldName: string }[];
 };
+
 
 const AuthForm = ({
   onSubmit,
@@ -18,38 +20,105 @@ const AuthForm = ({
   onPromptActionPress,
   fields,
 }: AuthFormProps) => {
+
+ const [values, setValues] = useState<Record<string, string>>({});
+ const [errors, setErrors] = useState<Record<string, string>>({});
+
+
+  //handles the form submission
   const handleFormSubmit = () => {
-    onSubmit();
+
+   //validate the form values and set errors
+  const errors = validateForm(values, fields);
+
+  if (errors) {
+    setErrors(errors);
+  } else {
+    // if no errors, call the onSubmit prop with the form values
+    onSubmit(values);
+  }
+  };
+  
+  //handles the input change for a specific field
+  const handleInputChange = (fieldName: string, value: string) => {
+    setValues((prevValues) => ({ ...prevValues, [fieldName]: value }));
+    setErrors((prevErrors) => ({ ...prevErrors, [fieldName]: '' }));
   };
 
+
+   //validates the form values and returns errors
+  const validateForm = (values: Record<string, string>, fields: { label: string; fieldName: string }[]) => {
+    let hasErrors = false;
+    
+    // Initializes a variable to store the error message for the field
+  const newErrors: Record<string, string> = {};
+
+  fields.forEach((field) => {
+    let error = '';
+    switch (field.fieldName.toLowerCase()) {
+      case 'username':
+        error = validateName(values[field.fieldName] || '');
+        break;
+      case 'email':
+        error = validateEmail(values[field.fieldName] || '');
+        break;
+      case 'password':
+        error = validatePassword(values[field.fieldName] || '');
+        break;
+      case 'confirm password':
+        error = validateConfirmPassword(values['Password'] || '', values[field.fieldName] || '');
+        break;
+      default:
+        error = validateRequired(values[field.fieldName] || '', field.label);
+    }
+
+    if (error) {
+      hasErrors = true;
+      newErrors[field.fieldName] = error;
+    }
+  });
+
+  return hasErrors ? newErrors : null;
+};
+
+  
   return (
-    <View style={styles.container}>
-      {fields.map((field) => (
-        <View key={field.fieldName} style={styles.fieldContainer}>
-          <Text>{field.label}</Text>
-          <TextInput
-            style={styles.input}
-            placeholder={field.fieldName}
-            secureTextEntry={field.fieldName.toLowerCase().includes('password')}
-          />
-        </View>
-      ))}
-      <TouchableOpacity onPress={handleFormSubmit} style={styles.button}>
-        <Text>{buttonText}</Text>
-      </TouchableOpacity>
-      <View>
-        <Text style={styles.promptText}>{promptText}</Text>
-        <TouchableOpacity onPress={onPromptActionPress}>
-          <Text>{PromptActionText}</Text>
+    <KeyboardAvoidingView behavior={Platform.OS === 'ios' ? 'padding' : 'height'} style={styles.keyboard}>
+      <View style={styles.container}>
+        {fields.map((field) => (
+          <View key={field.fieldName} style={styles.fieldContainer}>
+            <Text style={styles.label}>{field.label}</Text>
+            <TextInput
+              style={styles.input}
+              placeholder={field.fieldName}
+              secureTextEntry={field.fieldName.toLowerCase().includes('password')}
+              onChangeText={(value) => handleInputChange(field.fieldName, value)}
+            />
+            {errors[field.fieldName] && <Text style={styles.error}>{errors[field.fieldName]}</Text>}
+          </View>
+        ))}
+        <TouchableOpacity onPress={handleFormSubmit} style={styles.button}>
+          <Text style={styles.buttonText}>{buttonText}</Text>
         </TouchableOpacity>
+        <View style={styles.promptContainer}>
+          <Text>{promptText}</Text>
+          <TouchableOpacity onPress={onPromptActionPress}>
+            <Text style={styles.PromptActionText}>{PromptActionText}</Text>
+          </TouchableOpacity>
+        </View>
       </View>
-    </View>
+    </KeyboardAvoidingView>
   );
 };
 
-const styles = {
+const styles = StyleSheet.create({
+  keyboard: {
+    flex: 1,
+  },
   container: {
+    flex: 1,
     paddingHorizontal: 20,
+    paddingTop: 60,
   },
   fieldContainer: {
     marginBottom: 20,
@@ -79,12 +148,20 @@ const styles = {
   },
   promptContainer: {
     flexDirection: 'row',
-    justifyContent: 'flex-end',
-    alignItems: 'center',
+    position: 'absolute',
+    bottom: 0,
+    marginBottom: 20,
+    paddingHorizontal: 20,
   },
-  promptText: {
-    marginRight: 4,
+  PromptActionText: {
+    color: '#f7691a',
+    fontWeight: 'bold',
+    marginLeft: 5,
   },
-};
+  error: {
+    color: 'red',
+    fontSize: 12,
+  },
+});
 
 export default AuthForm;
---
+++ src/utils/validation.tsx
@@ -0,0 +1,50 @@
+/**
+ * Validation functions for signup/login form fields
+ */
+
+export const validateRequired = (value: string, label: string) => {
+  if (!value) {
+    return `${label} is required`;
+  }
+  return '';
+};
+
+export const validateName = (value: string) => {
+  if (!value) {
+    return 'Name is required';
+  }
+  if (value.length < 3) {
+    return 'Name must be at least 3 characters';
+  }
+  return '';
+};
+
+export const validateEmail = (value: string) => {
+  if (!value) {
+    return 'Email is required';
+  }
+  if (!/^[A-Z0-9._%+-]+@[A-Z0-9.-]+\.[A-Z]{2,}$/i.test(value)) {
+    return 'Invalid email address';
+  }
+  return '';
+};
+
+export const validatePassword = (value: string) => {
+  if (!value) {
+    return 'Password is required';
+  }
+  if (value.length < 8) {
+    return 'Password must be at least 8 characters';
+  }
+  return '';
+};
+
+export const validateConfirmPassword = (password: string, confirmPassword: string) => {
+  if (!confirmPassword) {
+    return 'Confirm Password is required';
+  }
+  if (password !== confirmPassword) {
+    return 'Passwords do not match';
+  }
+  return '';
+};
